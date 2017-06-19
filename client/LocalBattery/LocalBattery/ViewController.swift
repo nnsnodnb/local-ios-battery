3fa3c49800a73cd4ec618f425e3e052d491a181e
nnsnodnb <ahr63_gej@me.com>
2017-06-20 00:05:45 +0900

    Initial commit

---
+++ client/LocalBattery/LocalBattery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalBattery
+//
+//  Created by Oka Yuya on 2017/06/20.
+//  Copyright © 2017年 Oka Yuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
